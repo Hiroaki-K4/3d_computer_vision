5581a075c0c2231ad96b79e1da27b5a4faa61877
Hiroaki-K4 <49no19@gmail.com>
2023-03-29 09:36:15 +0900

    Update elliptic_fitting_by_least_squares.py

--- elliptic_fitting/elliptic_fitting_by_least_squares.py
+++ elliptic_fitting/elliptic_fitting_by_least_squares.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import sympy
 
 
 def plot_base():
@@ -19,7 +20,6 @@ def get_elliptic_points_with_tilt():
     for theta in range(360):
         point = np.array([7.5 * np.cos(np.deg2rad(theta)), 5 * np.sin(np.deg2rad(theta))])
         noise = np.random.normal(0, 0.2, point.shape)
-        print(noise)
         rotated_point = np.dot(R, point.T)
         x.append(rotated_point[0])
         y.append(rotated_point[1])
@@ -31,12 +31,34 @@ def get_elliptic_points_with_tilt():
     return x, y, n_x, n_y
 
 
+def elliptic_fitting_by_least_squares(noise_x, noise_y):
+    xi_sum = np.zeros((6, 6))
+    for i in range(len(noise_x)):
+        x = noise_x[i]
+        y = noise_y[i]
+        f = 20
+        xi = np.array([[x**2, 2*x*y, y**2, 2*f*x, 2*f*y, f*f]])
+        xi_sum += np.dot(xi.T, xi)
+        print("xi_sum: ", xi_sum)
+        # print(xi.T.shape)
+        # print(M)
+    M = xi_sum / len(noise_x)
+    w, v = np.linalg.eig(M)
+    min_eig_vec = v[:, np.argmin(w)]
+
+    return min_eig_vec
+
+
 def main():
     plot_base()
-    r_x, r_y, n_x, n_y = get_elliptic_points_with_tilt()
+    corr_x, corr_y, noise_x, noise_y = get_elliptic_points_with_tilt()
 
-    plt.scatter(r_x, r_y, marker='o', c="black", s=20)
-    plt.scatter(n_x, n_y, marker='o', c="blue", s=20)
+    theta = elliptic_fitting_by_least_squares(noise_x, noise_y)
+    # for i in range(corr_x):
+        
+
+    # plt.scatter(corr_x, corr_y, marker='o', c="black", s=20)
+    plt.scatter(noise_x, noise_y, marker='o', c="blue", s=20)
     plt.show()
 
 
