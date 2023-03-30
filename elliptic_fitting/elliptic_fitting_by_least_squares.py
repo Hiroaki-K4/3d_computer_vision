c5b20185fbc03e51f204c92759ec7bded695ae1c
Hiroaki-K4 <49no19@gmail.com>
2023-03-30 09:37:51 +0900

    Update elliptic_fitting_by_least_squares

--- elliptic_fitting/elliptic_fitting_by_least_squares.py
+++ elliptic_fitting/elliptic_fitting_by_least_squares.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import sympy
+from tqdm import tqdm
 
 
 def plot_base():
@@ -31,17 +32,14 @@ def get_elliptic_points_with_tilt():
     return x, y, n_x, n_y
 
 
-def elliptic_fitting_by_least_squares(noise_x, noise_y):
+def elliptic_fitting_by_least_squares(noise_x, noise_y, f):
     xi_sum = np.zeros((6, 6))
     for i in range(len(noise_x)):
         x = noise_x[i]
         y = noise_y[i]
-        f = 20
         xi = np.array([[x**2, 2*x*y, y**2, 2*f*x, 2*f*y, f*f]])
         xi_sum += np.dot(xi.T, xi)
-        print("xi_sum: ", xi_sum)
-        # print(xi.T.shape)
-        # print(M)
+
     M = xi_sum / len(noise_x)
     w, v = np.linalg.eig(M)
     min_eig_vec = v[:, np.argmin(w)]
@@ -53,12 +51,24 @@ def main():
     plot_base()
     corr_x, corr_y, noise_x, noise_y = get_elliptic_points_with_tilt()
 
-    theta = elliptic_fitting_by_least_squares(noise_x, noise_y)
-    # for i in range(corr_x):
-        
+    f_0 = 20
+    theta = elliptic_fitting_by_least_squares(noise_x, noise_y, f_0)
+    y = sympy.Symbol('y')
+    fit_x = []
+    fit_y = []
+    for x in tqdm(corr_x):
+        f = theta[0]*x**2 + 2*theta[1]*x*y + theta[2]*y**2 + 2*f_0*(theta[3]*x+theta[4]*y)+f_0**2*theta[5]
+        solutions=sympy.solve(f, y)
+        # print("solutions: ", solutions)
+        for y_ans in solutions:
+            if type(y_ans) == sympy.core.add.Add:
+                continue
+            fit_x.append(x)
+            fit_y.append(y_ans)
 
     # plt.scatter(corr_x, corr_y, marker='o', c="black", s=20)
     plt.scatter(noise_x, noise_y, marker='o', c="blue", s=20)
+    plt.scatter(fit_x, fit_y, marker='o', c="red", s=20)
     plt.show()
 
 
