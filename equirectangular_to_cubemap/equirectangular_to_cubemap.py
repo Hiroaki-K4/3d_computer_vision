fb426b18f911559f0377b2f8571a652910276f29
Hiroaki-K4 <49no19@gmail.com>
2023-05-20 10:52:05 +0900

    Create left and right image

--- equirectangular_to_cubemap/equirectangular_to_cubemap.py
+++ equirectangular_to_cubemap/equirectangular_to_cubemap.py
@@ -38,25 +38,61 @@ def create_equirectangler_to_bottom_and_top_map(input_w, input_h, output_sqr, z)
     return map_x, map_y
 
 
+def create_equirectangler_to_left_and_right_map(input_w, input_h, output_sqr, x):
+    map_x = np.zeros((output_sqr, output_sqr))
+    map_y = np.zeros((output_sqr, output_sqr))
+    for row in tqdm(range(output_sqr)):
+        for col in range(output_sqr):
+            z = float(row - output_sqr / 2.0)
+            y = float(col - output_sqr / 2.0)
+
+            rho = float(np.sqrt(x * x + y * y + x * x))
+            norm_theta = float(get_theta(x, y) / (2 * math.pi))
+            norm_phi = float((math.pi - np.arccos(z / rho)) / math.pi)
+            ix = float(norm_theta * input_w)
+            iy = float(norm_phi * input_h)
+
+            if input_w <= ix:
+                ix = ix - input_w
+            if input_h <= iy:
+                iy = iy - input_h
+
+            map_x[row, col] = ix
+            map_y[row, col] = iy
+
+    return map_x, map_y
+
+
 def main(image_path):
     img = cv2.imread(image_path)
     output_sqr = 400
     normalized_f = 1.0
+    input_w = img.shape[1]
+    input_h = img.shape[0]
 
     # Create bottom image
     z = float(output_sqr / (2.0 * normalized_f))
-    bottom_map_x , bottom_map_y = create_equirectangler_to_bottom_and_top_map(img.shape[1], img.shape[0], output_sqr, z)
+    bottom_map_x , bottom_map_y = create_equirectangler_to_bottom_and_top_map(input_w, input_h, output_sqr, z)
     bottom_img = cv2.remap(img, bottom_map_x.astype('float32'), bottom_map_y.astype('float32'), cv2.INTER_CUBIC)
     cv2.imwrite("bottom.png", bottom_img)
 
     # Create top image
     z = -float(output_sqr / (2.0 * normalized_f))
-    top_map_x , top_map_y = create_equirectangler_to_bottom_and_top_map(img.shape[1], img.shape[0], output_sqr, z)
+    top_map_x , top_map_y = create_equirectangler_to_bottom_and_top_map(input_w, input_h, output_sqr, z)
     top_img = cv2.remap(img, top_map_x.astype('float32'), top_map_y.astype('float32'), cv2.INTER_CUBIC)
     cv2.imwrite("top.png", top_img)
 
     # Create left image
+    x = -float(output_sqr / (2.0 * normalized_f))
+    top_map_x , top_map_y = create_equirectangler_to_left_and_right_map(input_w, input_h, output_sqr, x)
+    top_img = cv2.remap(img, top_map_x.astype('float32'), top_map_y.astype('float32'), cv2.INTER_CUBIC)
+    cv2.imwrite("left.png", top_img)
 
+    # Create right image
+    x = float(output_sqr / (2.0 * normalized_f))
+    right_map_x , right_map_y = create_equirectangler_to_left_and_right_map(input_w, input_h, output_sqr, x)
+    right_img = cv2.remap(img, right_map_x.astype('float32'), right_map_y.astype('float32'), cv2.INTER_CUBIC)
+    cv2.imwrite("right.png", right_img)
 
 
 if __name__ == '__main__':
