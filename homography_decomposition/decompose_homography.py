2e561c455f35cad5c4f84c56d7b543e4ed8954a7
Hiroaki-K4 <49no19@gmail.com>
2023-09-06 08:47:37 +0900

    Update homography decomposition

--- homography_decomposition/decompose_homography.py
+++ homography_decomposition/decompose_homography.py
@@ -16,11 +16,34 @@ def decompose_homography(H, f):
 
     H_new = np.dot(np.dot(f_0, H), f_1)
     print("H_new: ", H_new)
-    H_norm = H_new / np.linalg.norm(H_new)
-    print(H_norm)
+    H_norm = H_new / np.linalg.det(H_new) ** 1/3
+    print(np.linalg.det(H_norm))
+    input()
     U, S, Vt = np.linalg.svd(H_norm)
-    print(Vt)
-    # v_1 = 
+    V = Vt.T
+    print("V", V)
+    v_1 = V[:, 0]
+    v_2 = V[:, 1]
+    v_3 = V[:, 2]
+    s_1 = S[0]
+    s_2 = S[1]
+    s_3 = S[2]
+    print(s_1)
+    n_1 = np.sqrt(s_1 ** 2 - s_2 ** 2) * v_1 + np.sqrt(s_2 ** 2 - s_3 ** 2) * v_3
+    n_2 = np.sqrt(s_1 ** 2 - s_2 ** 2) * v_1 - np.sqrt(s_2 ** 2 - s_3 ** 2) * v_3
+    n_1 = n_1 / np.linalg.norm(n_1)
+    n_2 = n_2 / np.linalg.norm(n_2)
+    h = s_2 / (s_1 - s_3)
+    print(n_1)
+    print(n_2)
+    print(h)
+    t_1 = -s_3 * np.sqrt(s_1 ** 2 - s_2 ** 2) * v_1 + s_1 * np.sqrt(s_2 ** 2 - s_3 ** 2) * v_3
+    t_2 = -s_3 * np.sqrt(s_1 ** 2 - s_2 ** 2) * v_1 - s_1 * np.sqrt(s_2 ** 2 - s_3 ** 2) * v_3
+    t_1 = t_1 / np.linalg.norm(t_1)
+    t_2 = t_2 / np.linalg.norm(t_2)
+    print(t_1)
+    print(t_2)
+    # R = 
     input()
 
 
