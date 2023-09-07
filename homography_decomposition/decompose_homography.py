9400e4e72a2c605115192b70b7d88bcfb8a6bac2
Hiroaki-K4 <49no19@gmail.com>
2023-09-07 09:16:30 +0900

    Update homography decomposition

--- homography_decomposition/decompose_homography.py
+++ homography_decomposition/decompose_homography.py
@@ -1,4 +1,5 @@
 import sys
+
 import cv2
 import numpy as np
 
@@ -7,44 +8,59 @@ from prepare_test_data_utils import prepare_test_data_for_fundamental_matrix
 
 
 def decompose_homography(H, f):
-    f_0 = np.array([[1, 0, 0],
-                    [0, 1, 0],
-                    [0, 0, f]])
-    f_1 = np.array([[1, 0, 0],
-                    [0, 1, 0],
-                    [0, 0, 1/f]])
+    f_0 = np.array([[1, 0, 0], [0, 1, 0], [0, 0, f]])
+    f_1 = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1 / f]])
 
     H_new = np.dot(np.dot(f_0, H), f_1)
-    print("H_new: ", H_new)
-    H_norm = H_new / np.linalg.det(H_new) ** 1/3
-    print(np.linalg.det(H_norm))
-    input()
+    H_norm = H_new / np.cbrt(np.linalg.det(H_new))
     U, S, Vt = np.linalg.svd(H_norm)
     V = Vt.T
-    print("V", V)
     v_1 = V[:, 0]
     v_2 = V[:, 1]
     v_3 = V[:, 2]
     s_1 = S[0]
     s_2 = S[1]
     s_3 = S[2]
-    print(s_1)
-    n_1 = np.sqrt(s_1 ** 2 - s_2 ** 2) * v_1 + np.sqrt(s_2 ** 2 - s_3 ** 2) * v_3
-    n_2 = np.sqrt(s_1 ** 2 - s_2 ** 2) * v_1 - np.sqrt(s_2 ** 2 - s_3 ** 2) * v_3
-    n_1 = n_1 / np.linalg.norm(n_1)
-    n_2 = n_2 / np.linalg.norm(n_2)
+    n_1 = np.sqrt(s_1**2 - s_2**2) * v_1 + np.sqrt(s_2**2 - s_3**2) * v_3
+    n_2 = np.sqrt(s_1**2 - s_2**2) * v_1 - np.sqrt(s_2**2 - s_3**2) * v_3
+    n_1_1 = n_1 / np.linalg.norm(n_1)
+    n_1_2 = -n_1_1
+    n_2_1 = n_2 / np.linalg.norm(n_2)
+    n_2_2 = -n_2_1
+    normal_vecs = [n_1_1, n_1_2, n_2_1, n_2_2]
     h = s_2 / (s_1 - s_3)
-    print(n_1)
-    print(n_2)
-    print(h)
-    t_1 = -s_3 * np.sqrt(s_1 ** 2 - s_2 ** 2) * v_1 + s_1 * np.sqrt(s_2 ** 2 - s_3 ** 2) * v_3
-    t_2 = -s_3 * np.sqrt(s_1 ** 2 - s_2 ** 2) * v_1 - s_1 * np.sqrt(s_2 ** 2 - s_3 ** 2) * v_3
-    t_1 = t_1 / np.linalg.norm(t_1)
-    t_2 = t_2 / np.linalg.norm(t_2)
-    print(t_1)
-    print(t_2)
-    # R = 
-    input()
+    t_1 = (
+        -s_3 * np.sqrt(s_1**2 - s_2**2) * v_1
+        + s_1 * np.sqrt(s_2**2 - s_3**2) * v_3
+    )
+    t_2 = (
+        -s_3 * np.sqrt(s_1**2 - s_2**2) * v_1
+        - s_1 * np.sqrt(s_2**2 - s_3**2) * v_3
+    )
+    t_1_1 = t_1 / np.linalg.norm(t_1)
+    t_1_2 = -t_1_1
+    t_2_1 = t_2 / np.linalg.norm(t_2)
+    t_2_2 = -t_2_1
+    trans_vecs = [t_1_1, t_1_2, t_2_1, t_2_2]
+    rotations = []
+    for idx in range(len(normal_vecs)):
+        R = (
+            1
+            / s_2
+            * np.dot(
+                (np.identity(3) + s_2**3 * normal_vecs[idx] * trans_vecs[idx].T / h),
+                H_norm.T,
+            )
+        )
+        rotations.append(R)
+
+    return (
+        len(normal_vecs),
+        np.array(rotations),
+        np.array(trans_vecs),
+        np.array(normal_vecs),
+        h,
+    )
 
 
 def main():
@@ -67,22 +83,14 @@ def main():
     print(H_cv)
 
     f = 160
-    width = 640
-    height = 480
-    pp = (width / 2, height / 2)
-    K = np.matrix(
-        np.array([[f, 0, pp[0]], [0, f, pp[1]], [0, 0, 1]], dtype="double")
-    )
+    num, Rs, Ts, Ns, h = decompose_homography(H_cv, f)
 
-    decompose_homography(H_cv, f)
-
-    num, Rs, Ts, Ns  = cv2.decomposeHomographyMat(H_cv, K)
-    print(num)
-    print(Rs)
-    print(Ts)
-    print(Ns)
+    print("num: ", num)
+    print("h: ", h)
+    print("Rs: ", Rs)
+    print("Ts: ", Ts)
+    print("Ns: ", Ns)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
