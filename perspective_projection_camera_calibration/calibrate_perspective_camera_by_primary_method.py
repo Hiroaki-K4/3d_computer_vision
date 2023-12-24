98d3eeec2eefe637476f1186a8c3cbec1ca9d10a
Hiroaki-K4 <49no19@gmail.com>
2023-12-24 14:05:04 +0900

    Update perspective camera calibration

--- perspective_projection_camera_calibration/calibrate_perspective_camera_by_primary_method.py
+++ perspective_projection_camera_calibration/calibrate_perspective_camera_by_primary_method.py
@@ -15,19 +15,53 @@ def normalize_each_column(mat):
     return norm_mat
 
 
-def calibrate_perspective_camera_by_primary_method(img_pnts_list, f_0):
-    A = np.empty(len(img_pnts_list))
-    W = np.empty((3 * len(img_pnts_list), len(img_pnts_list[0])))
-    Z = np.ones((len(img_pnts_list), len(img_pnts_list[0])))
-    print(Z)
-    print(Z.shape)
+def calculate_mat_for_decreasing_error(
+    img_pnts_list, U, point_idx, frm_idx_1, frm_idx_2, f_0
+):
+    A = 0
+    x_1 = np.ones(3)
+    x_1[0] = img_pnts_list[frm_idx_1][point_idx][0] / f_0
+    x_1[1] = img_pnts_list[frm_idx_1][point_idx][1] / f_0
+    x_2 = np.ones(3)
+    x_2[0] = img_pnts_list[frm_idx_2][point_idx][0] / f_0
+    x_2[1] = img_pnts_list[frm_idx_2][point_idx][1] / f_0
+    x_norm = np.linalg.norm(x_1) * np.linalg.norm(x_2)
+    u_1 = np.empty(3)
+    u_2 = np.empty(3)
+    for i in range(4):
+        u_1[0] = U[3 * frm_idx_1][i]
+        u_1[1] = U[3 * frm_idx_1 + 1][i]
+        u_1[2] = U[3 * frm_idx_1 + 2][i]
+        u_2[0] = U[3 * frm_idx_2][i]
+        u_2[1] = U[3 * frm_idx_2 + 1][i]
+        u_2[2] = U[3 * frm_idx_2 + 2][i]
+        A += np.dot(x_1, u_1) * np.dot(x_2, u_2) / x_norm
+
+    return A
+
+
+def update_projective_depth(Z, img_pnts_list, xi, point_idx, f_0):
+    x = np.ones(3)
+    for frm_idx in range(len(img_pnts_list)):
+        x[0] = img_pnts_list[frm_idx][point_idx][0] / f_0
+        x[1] = img_pnts_list[frm_idx][point_idx][1] / f_0
+        Z[frm_idx][point_idx] = xi[frm_idx] / np.linalg.norm(x)
+
+
+def update_observation_matrix(W, Z, img_pnts_list, f_0):
     for i in range(len(img_pnts_list)):
         for j in range(len(img_pnts_list[0])):
             point = img_pnts_list[i][j]
-
             W[i * 3][j] = Z[i][j] * point[0] / f_0
             W[i * 3 + 1][j] = Z[i][j] * point[1] / f_0
             W[i * 3 + 2][j] = Z[i][j]
+
+
+def calibrate_perspective_camera_by_primary_method(img_pnts_list, f_0):
+    A = np.empty((len(img_pnts_list), len(img_pnts_list)))
+    W = np.empty((3 * len(img_pnts_list), len(img_pnts_list[0])))
+    Z = np.ones((len(img_pnts_list), len(img_pnts_list[0])))
+    update_observation_matrix(W, Z, img_pnts_list, f_0)
 
     norm_W = normalize_each_column(W)
     print("norm_W: ", norm_W)
@@ -35,10 +69,24 @@ def calibrate_perspective_camera_by_primary_method(img_pnts_list, f_0):
 
     U, S, Vt = np.linalg.svd(norm_W)
     U = U[:, :4]
-    print(U)
-    print(U.shape)
-    # S = np.array([[S[0], 0, 0], [0, S[1], 0], [0, 0, S[2]]])
-    # V = Vt.T[:, :3]
+
+    for point_idx in range(len(img_pnts_list[0])):
+        for frm_idx_1 in range(len(img_pnts_list)):
+            for frm_idx_2 in range(len(img_pnts_list)):
+                A[frm_idx_1][frm_idx_2] = calculate_mat_for_decreasing_error(
+                    img_pnts_list, U, point_idx, frm_idx_1, frm_idx_2, f_0
+                )
+
+        w, v = np.linalg.eig(A)
+        xi = v[:, np.argmax(w)]
+        if sum(xi) < 0:
+            xi = (-1) * xi
+        update_projective_depth(Z, img_pnts_list, xi, point_idx, f_0)
+
+    print(Z)
+    input()
+
+    # TODO Add Camera matrix(P) and postion(X)
 
     # B_all = np.zeros((3, 3, 3, 3))
     # for i in range(B_all.shape[0]):
