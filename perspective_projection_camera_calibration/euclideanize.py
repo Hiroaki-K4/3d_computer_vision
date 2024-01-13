8fccbe351778caff6a7d3f006528e79784f6ffd9
Hiroaki-K4 <49no19@gmail.com>
2024-01-13 15:14:51 +0900

    Update euclideanization

--- perspective_projection_camera_calibration/euclideanize.py
+++ perspective_projection_camera_calibration/euclideanize.py
@@ -1,4 +1,5 @@
 import sys
+
 import numpy as np
 
 from calibrate_perspective_camera_by_primary_method import (
@@ -12,16 +13,87 @@ from prepare_test_data_utils import prepare_test_data
 
 def calculate_mat_including_homography_mat(K, motion_mat):
     print(K.shape)
-    print(motion_mat.shape)
+    print(motion_mat)
+    # input()
     img_num = int(motion_mat.shape[0] / 3)
-    for i in range(img_num):
-        print(i)
-        # TODO Calcualte omega
+    A_k = np.zeros((4, 4, 4, 4))
+    for idx in range(img_num):
+        P = motion_mat[idx * 3 : idx * 3 + 3, :]
+        Q = np.dot(np.linalg.inv(K[idx]), P)
+        print(Q)
+        for i in range(4):
+            for j in range(4):
+                for k in range(4):
+                    for l in range(4):
+                        A_k[i][j][k][l] += (
+                            Q[0][i] * Q[0][j] * Q[0][k] * Q[0][l]
+                            - Q[0][i] * Q[0][j] * Q[1][k] * Q[1][l]
+                            - Q[1][i] * Q[1][j] * Q[0][k] * Q[0][l]
+                            + Q[1][i] * Q[1][j] * Q[1][k] * Q[1][l]
+                            + 1
+                            / 4
+                            * (
+                                (Q[0][i] * Q[1][j] * Q[0][k] * Q[1][l])
+                                + (Q[1][i] * Q[0][j] * Q[0][k] * Q[1][l])
+                                + (Q[0][i] * Q[1][j] * Q[1][k] * Q[1][l])
+                                + (Q[1][i] * Q[0][j] * Q[1][k] * Q[0][l])
+                            )
+                            + 1
+                            / 4
+                            * (
+                                (Q[1][i] * Q[2][j] * Q[1][k] * Q[2][l])
+                                + (Q[2][i] * Q[1][j] * Q[1][k] * Q[2][l])
+                                + (Q[1][i] * Q[2][j] * Q[2][k] * Q[1][l])
+                                + (Q[2][i] * Q[1][j] * Q[2][k] * Q[1][l])
+                            )
+                            + 1
+                            / 4
+                            * (
+                                (Q[2][i] * Q[0][j] * Q[2][k] * Q[0][l])
+                                + (Q[0][i] * Q[2][j] * Q[2][k] * Q[0][l])
+                                + (Q[2][i] * Q[0][j] * Q[0][k] * Q[2][l])
+                                + (Q[0][i] * Q[2][j] * Q[0][k] * Q[2][l])
+                            )
+                        )
+
+    # TODO Calculate A mat
+    A = np.array(
+        [
+            [
+                A_k[0][0][0][0],
+                A_k[0][0][1][1],
+                A_k[0][0][2][2],
+                A_k[0][0][3][3],
+                np.sqrt(2) * A_k[0][0][0][1],
+                np.sqrt(2) * A_k[0][0][0][2],
+                np.sqrt(2) * A_k[0][0][0][3],
+                np.sqrt(2) * A_k[0][0][1][2],
+                np.sqrt(2) * A_k[0][0][1][3],
+                np.sqrt(2) * A_k[0][0][2][3],
+            ],
+            [
+                A_k[1][1][0][0],
+                A_k[1][1][1][1],
+                A_k[1][1][2][2],
+                A_k[1][1][3][3],
+                np.sqrt(2) * A_k[1][1][0][1],
+                np.sqrt(2) * A_k[1][1][0][2],
+                np.sqrt(2) * A_k[1][1][0][3],
+                np.sqrt(2) * A_k[1][1][1][2],
+                np.sqrt(2) * A_k[1][1][1][3],
+                np.sqrt(2) * A_k[1][1][2][3],
+            ],
+        ]
+    )
+    print(A_k)
+    input()
 
 
 def euclideanize(motion_mat, shape_mat, f, f_0, opt_axis):
     J_med = sys.float_info.max
     img_num = int(motion_mat.shape[0] / 3)
+
+    # Initialize intrinsic parameter K
     K = np.empty((img_num, 3, 3))
     for i in range(img_num):
         K_new = np.array([[f, 0, opt_axis[0]], [0, f, opt_axis[1]], [0, 0, f_0]])
