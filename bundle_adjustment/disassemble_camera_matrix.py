93f440d8d8e16ad89027d9803b08337f99723a1a
Hiroaki-K4 <49no19@gmail.com>
2024-02-19 09:37:42 +0900

    Update disassemble_camera_matrix

--- bundle_adjustment/disassemble_camera_matrix.py
+++ bundle_adjustment/disassemble_camera_matrix.py
@@ -1,5 +1,23 @@
 import json
+import os
+
 import numpy as np
+import scipy
+
+
+def disassemble_camera_matrix(P):
+    Q = P[:, :3]
+    q = P[:, 3]
+    if np.linalg.det(Q) < 0:
+        Q = (-1) * Q
+        q = (-1) * q
+
+    t = np.dot(-np.linalg.inv(Q), q)
+    C = scipy.linalg.cholesky(np.linalg.inv(np.dot(Q, Q.T)))
+    K = np.linalg.inv(C)
+    R = np.dot(Q.T, C.T)
+
+    return K, R, t
 
 
 def main(camera_matrix_file):
@@ -7,9 +25,21 @@ def main(camera_matrix_file):
         camera_mat = json.load(f)
 
     camera_p = camera_mat["P"]
-
+    params = []
     for idx in range(len(camera_p)):
-        print(np.array(camera_p[idx]).shape)
+        P = np.array(camera_p[idx])
+        K, R, t = disassemble_camera_matrix(P)
+        param = {}
+        param["K"] = K.tolist()
+        param["R"] = R.tolist()
+        param["t"] = t.tolist()
+        params.append(param)
+
+    output_path = os.path.join(
+        os.path.dirname(camera_matrix_file), "camera_parameters.json"
+    )
+    with open(output_path, "w") as f:
+        json.dump(params, f, indent=4)
 
 
 if __name__ == "__main__":
