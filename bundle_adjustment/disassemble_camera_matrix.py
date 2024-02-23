3f5dd4e4fcbd5374808e911c31cae74116c4e7ec
Hiroaki-K4 <49no19@gmail.com>
2024-02-23 13:57:34 +0900

    Fix conflict

--- bundle_adjustment/disassemble_camera_matrix.py
+++ bundle_adjustment/disassemble_camera_matrix.py
@@ -1,12 +1,8 @@
 import json
 import os
-<<<<<<< HEAD
 import sys
 
 import matplotlib.pyplot as plt
-=======
-
->>>>>>> 333071230be03124b5c6b5ee0da8ebf71bb2e254
 import numpy as np
 import scipy
 
@@ -26,17 +22,12 @@ def disassemble_camera_matrix(P):
     return K, R, t
 
 
-<<<<<<< HEAD
 def main(camera_matrix_file, show_flag):
-=======
-def main(camera_matrix_file):
->>>>>>> 333071230be03124b5c6b5ee0da8ebf71bb2e254
     with open(camera_matrix_file) as f:
         camera_mat = json.load(f)
 
     camera_p = camera_mat["P"]
     params = []
-<<<<<<< HEAD
     show_pos_x = []
     show_pos_y = []
     show_pos_z = []
@@ -46,11 +37,6 @@ def main(camera_matrix_file):
         show_pos_x.append(t[0])
         show_pos_y.append(t[1])
         show_pos_z.append(t[2])
-=======
-    for idx in range(len(camera_p)):
-        P = np.array(camera_p[idx])
-        K, R, t = disassemble_camera_matrix(P)
->>>>>>> 333071230be03124b5c6b5ee0da8ebf71bb2e254
         param = {}
         param["K"] = K.tolist()
         param["R"] = R.tolist()
@@ -63,7 +49,6 @@ def main(camera_matrix_file):
     with open(output_path, "w") as f:
         json.dump(params, f, indent=4)
 
-<<<<<<< HEAD
     if show_flag:
         fig = plt.figure()
         ax = fig.add_subplot(projection="3d")
@@ -83,11 +68,3 @@ if __name__ == "__main__":
         "/home/hiroakik4/mypro2/3d_computer_vision/bundle_adjustment/dinosaur.json"
     )
     main(camera_matrix_file, show_flag)
-=======
-
-if __name__ == "__main__":
-    camera_matrix_file = (
-        "/home/hiroakik4/mypro2/3d_computer_vision/bundle_adjustment/dinosaur.json"
-    )
-    main(camera_matrix_file)
->>>>>>> 333071230be03124b5c6b5ee0da8ebf71bb2e254
